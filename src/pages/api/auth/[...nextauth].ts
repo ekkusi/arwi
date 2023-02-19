3083d422d922766bcfda93043c9ac0e756ad580f
Ekku Sipilä <ekku.eki@gmail.com>
2023-02-20 00:59:47 +0200

    Better configuration for redirect

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -121,9 +121,11 @@ export const authOptions: AuthOptions = {
       };
     },
     async redirect({ url, baseUrl }) {
-      return url.startsWith(baseUrl)
-        ? Promise.resolve(url)
-        : Promise.resolve(baseUrl);
+      // Allows relative callback URLs
+      if (url.startsWith("/")) return `${baseUrl}${url}`;
+      // Allows callback URLs on the same origin
+      else if (new URL(url).origin === baseUrl) return url;
+      return baseUrl;
     },
   },
   debug: true,
