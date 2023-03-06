caafe30be6552cd439991f5bfa87c367082ef7bf
Ekku Sipilä <ekku.eki@gmail.com>
2023-03-06 14:35:01 +0200

    Add .well-known folder to not run auth middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { withAuth } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|icons|manifest.json).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|icons|manifest.json|.well-known).*)",
   ],
 };
 
