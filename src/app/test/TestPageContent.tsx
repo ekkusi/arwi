fc74d9bbf85aac6cea5ce6213097edd22081e8b5
Ekku Sipilä <ekku.eki@gmail.com>
2023-03-16 10:03:54 +0200

    Refactor speech recognition into hook

--- src/app/test/TestPageContent.tsx
+++ src/app/test/TestPageContent.tsx
@@ -3,67 +3,19 @@
 "use client";
 
 import { Box, Button, Text } from "@/components/chakra";
+import useSpeechRecognition from "@/hooks-and-providers/useSpeechRecognition";
 import { useEffect, useState } from "react";
 
 export default function TestPageContent() {
-  const [speechRecognitionActive, setSpeechRecognitionActive] = useState(false);
-  const [speechRecognitionResult, setSpeechRecognitionResult] = useState("");
-  const [speechRecognition, setSpeechRecognition] = useState<
-    SpeechRecognition | undefined
-  >();
-
-  useEffect(() => {
-    if ("webkitSpeechRecognition" in window && !speechRecognition) {
-      const SpeechRecognition =
-        window.SpeechRecognition || webkitSpeechRecognition;
-      const recognition = new SpeechRecognition();
-      recognition.lang = "fi-FI";
-      recognition.continuous = true;
-      recognition.interimResults = true;
-      recognition.onresult = (event) => {
-        // Create the interim transcript string locally because we don't want it to persist like final transcript
-        console.log("Running onresult");
-
-        let result = "";
-        // Loop through the results from the speech recognition object.
-        for (let i = event.resultIndex; i < event.results.length; i += 1) {
-          // If the result item is Final, add it to Final Transcript, Else add it to Interim transcript
-          console.log("Index of result: ", i);
-
-          if (event.results[i].isFinal) {
-            const newText = event.results[i][0].transcript;
-            result = newText;
-            console.log("iS FINAL", newText);
-            break;
-          } else {
-            const newText = event.results[i][0].transcript;
-            console.log("iS interim", newText);
-            result += newText;
-          }
-        }
-        console.log("Setting result to: ", result);
-        setSpeechRecognitionResult(result);
-      };
-      recognition.onend = (event) => {
-        console.log("Ending speech recognition");
-
-        setSpeechRecognitionActive(false);
-      };
-      recognition.onstart = (event) => {
-        console.log("Starting speech recognition");
-      };
-      setSpeechRecognition(recognition);
-    }
-  }, [speechRecognition]);
+  const { speechRecognition, active, result } = useSpeechRecognition();
 
   const toggleSpeechRecognition = () => {
     if (!speechRecognition) return;
     console.log("toggleSpeechRecognition");
     if ("webkitSpeechRecognition" in window) {
       // Speech Recognition Stuff goes here
-      if (speechRecognitionActive) speechRecognition.stop();
+      if (active) speechRecognition.stop();
       else speechRecognition.start();
-      setSpeechRecognitionActive(!speechRecognitionActive);
     } else {
       console.error("Speech Recognition Not Available");
     }
@@ -76,12 +28,12 @@ export default function TestPageContent() {
         <Text as="h2">Speech recognition</Text>
         <Button
           onClick={toggleSpeechRecognition}
-          colorScheme={speechRecognitionActive ? "red" : "green"}
+          colorScheme={active ? "red" : "green"}
         >
-          {speechRecognitionActive ? "Stop" : "Record"}
+          {active ? "Stop" : "Record"}
         </Button>
         <Text as="h3">Result:</Text>
-        <Text>{speechRecognitionResult}</Text>
+        <Text>{result}</Text>
       </Box>
     </Box>
   );
---
+++ src/hooks-and-providers/useSpeechRecognition.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+
+type UseSpeechRecognitionProps = {
+  onResult?: (result: string) => void;
+};
+
+export default function useSpeechRecognition(
+  props?: UseSpeechRecognitionProps
+) {
+  const [active, setActive] = useState(false);
+  const [result, setResult] = useState("");
+  const [speechRecognition, setSpeechRecognition] = useState<
+    SpeechRecognition | undefined
+  >();
+
+  useEffect(() => {
+    if ("webkitSpeechRecognition" in window && !speechRecognition) {
+      // eslint-disable-next-line no-console
+      console.log("Setting up speech recognition");
+
+      const SpeechRecognition =
+        window.SpeechRecognition || webkitSpeechRecognition;
+      const recognition = new SpeechRecognition();
+      recognition.lang = "fi-FI";
+      recognition.continuous = true;
+      recognition.interimResults = true;
+      recognition.onresult = (event) => {
+        let tempResult = "";
+        // Loop through the results from the speech recognition object.
+        for (let i = event.resultIndex; i < event.results.length; i += 1) {
+          // If result is final, stop the recognition.
+          if (event.results[i].isFinal) {
+            const newText = event.results[i][0].transcript;
+            tempResult = newText;
+            recognition.stop();
+            break;
+          } else {
+            const newText = event.results[i][0].transcript;
+            tempResult += newText;
+          }
+        }
+        setResult(tempResult);
+        if (props?.onResult) {
+          props.onResult(tempResult);
+        }
+      };
+      recognition.onend = () => {
+        setActive(false);
+      };
+      recognition.onstart = () => {
+        setActive(true);
+      };
+      setSpeechRecognition(recognition);
+    }
+  }, [props, speechRecognition]);
+
+  return { speechRecognition, active, result };
+}
