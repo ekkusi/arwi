bc70b63bb3b51efe06a37e30bf1e4efb36652ab8
Ekku Sipilä <ekku.eki@gmail.com>
2023-03-19 14:35:04 +0200

    Modifications to front page styles, dont show create group button when user has groups already. Show minimalistic add icon instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { graphql } from "@/gql";
-import { Box, Button, NextLink, Text } from "@/components/chakra";
+import {
+  Box,
+  Button,
+  Flex,
+  IconButton,
+  NextLink,
+  Text,
+} from "@/components/chakra";
 import { serverRequest } from "@/pages/api/graphql";
 import PageWrapper from "@/app/(server-components)/PageWrapper";
 import { getSessionOrRedirect } from "@/utils/session/server";
 import GroupList from "@/components/functional/GroupList";
+import { IoAddSharp } from "react-icons/io5";
 
 // Necessary for revalidation to work
 export const dynamic = "force-dynamic";
@@ -35,18 +43,33 @@ export default async function HomePage() {
         <Text as="h1" fontSize="lg" fontStyle="italic" textAlign="center">
           ARWI
         </Text>
-        <Text as="h2" fontSize="lg" mb="2">
-          Omat ryhmät:
-        </Text>
+        <Flex mb="2" justifyContent="space-between" alignItems="center">
+          <Text as="h2" fontSize="lg" mb="0">
+            Omat ryhmät:
+          </Text>
+          <IconButton
+            variant="link"
+            size="lg"
+            as={NextLink}
+            icon={<IoAddSharp />}
+            href="/group/create"
+            ml="auto"
+            aria-label="Luo uusi luokka"
+          />
+        </Flex>
         {teacher.groups.length > 0 ? (
           <GroupList groups={teacher.groups} mb="5" />
         ) : (
-          <Text>Et vielä ole tehnyt ryhmiä</Text>
+          <>
+            <Text mb="3" mt="5">
+              Sinulla ei vielä ole ryhmiä
+            </Text>
+            <Button as={NextLink} href="/group/create" width="100%">
+              Luo ensimmäinen ryhmä
+            </Button>
+          </>
         )}
       </Box>
-      <Button as={NextLink} href="/group/create" width="100%">
-        {teacher.groups.length > 0 ? "Luo uusi ryhmä" : "Luo ryhmä"}
-      </Button>
     </PageWrapper>
   );
 }
