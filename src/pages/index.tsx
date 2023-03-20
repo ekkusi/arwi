db24067912a6d2b2e4c0a90cfc783202873947db
Ekku Sipilä <ekku.eki@gmail.com>
2023-03-20 22:39:02 +0200

    Fix next.config, move cache rules directly to config file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import { graphql } from "@/gql";
 import { Box, Button, Flex, IconButton, Text } from "@chakra-ui/react";
 import { serverRequest } from "@/pages/api/graphql";
@@ -24,20 +23,21 @@ const MainPage_GetTeacher_Query = graphql(`
 `);
 
 type MainPageProps = {
-  data: any;
+  data: MainPage_GetTeacherQuery;
 };
 
 export default function HomePage({ data }: MainPageProps) {
+  const { getTeacher: teacher } = data;
   // eslint-disable-next-line
-  console.log("data in home page", data);
+  console.log("teacher", teacher);
 
-  // if (!teacher) throw new Error("Unexpected error, no teacher");
+  if (!teacher) throw new Error("Unexpected error, no teacher");
 
   return (
     <PageWrapper display="flex" flexDirection="column">
       <Box mb="5">
         <Text as="h1" fontSize="lg" fontStyle="italic" textAlign="center">
-          ARWI - {data?.some}
+          ARWI
         </Text>
         <Flex mb="2" justifyContent="space-between" alignItems="center">
           <Text as="h2" fontSize="lg" mb="0">
@@ -53,7 +53,7 @@ export default function HomePage({ data }: MainPageProps) {
             aria-label="Luo uusi luokka"
           />
         </Flex>
-        {/* {teacher.groups.length > 0 ? (
+        {teacher.groups.length > 0 ? (
           <GroupList groups={teacher.groups} mb="5" />
         ) : (
           <>
@@ -64,7 +64,7 @@ export default function HomePage({ data }: MainPageProps) {
               Luo ensimmäinen ryhmä
             </Button>
           </>
-        )} */}
+        )}
       </Box>
     </PageWrapper>
   );
@@ -80,16 +80,16 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 
   // eslint-disable-next-line
   console.log("getting data");
-  const data = { some: "data" };
-  // try {
-  //   data = await serverRequest(MainPage_GetTeacher_Query, {
-  //     teacherId: session.user.id,
-  //   });
-  //   // eslint-disable-next-line
-  //   console.log("data in home page getServerSideProps", data);
-  // } catch (error) {
-  //   console.error("Error in home page getServerSideProps: ", error);
-  // }
+  let data;
+  try {
+    data = await serverRequest(MainPage_GetTeacher_Query, {
+      teacherId: session.user.id,
+    });
+    // eslint-disable-next-line
+    console.log("data in home page getServerSideProps", data);
+  } catch (error) {
+    console.error("Error in home page getServerSideProps: ", error);
+  }
 
   // Pass data to the page via props
   return { props: { data } };
