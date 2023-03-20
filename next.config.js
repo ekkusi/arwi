51e4ef0c9ca0051f8633040dd9e67cefe95364ac
Ekku Sipilä <ekku.eki@gmail.com>
2023-03-20 22:20:29 +0200

    Try remove service worker

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-});
+// const withPWA = require("@ducanh2912/next-pwa").default({
+//   dest: "public",
+// });
 
-const nextConfig = withPWA({
+// const nextConfig = withPWA({
+//   output: "standalone",
+// });
+
+const nextConfig = {
   output: "standalone",
-});
+}
 
 module.exports = nextConfig
