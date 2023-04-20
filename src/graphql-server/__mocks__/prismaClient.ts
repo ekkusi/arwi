45f033a7fdabed2bc28a7f35de775afd75bfcc90
Ekku Sipilä <ekku.eki@gmail.com>
2023-04-20 20:08:10 +0300

    Fix SQL syntax error

--- src/graphql-server/__mocks__/prismaClient.ts
+++ src/graphql-server/__mocks__/prismaClient.ts
@@ -21,9 +21,8 @@ export const prisma = new PrismaClient({
 
 beforeAll(async () => {
   await prisma.$executeRawUnsafe(`
-    SET search_path TO ${schemaId}};
+    SET search_path TO ${schemaId};
     DROP EXTENSION IF EXISTS "uuid-ossp";
-
     CREATE EXTENSION "uuid-ossp" SCHEMA ${schemaId};
   `);
   execSync(`npx prisma migrate dev`, {
