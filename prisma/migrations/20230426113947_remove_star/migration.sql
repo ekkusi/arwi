e1a1d2699769ebed1012fb454a8394845664ff05
Ekku Sipilä <ekku.eki@gmail.com>
2023-04-27 13:13:25 +0300

    Delete test migrations

--- prisma/migrations/20230426113947_remove_star/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `hasStar` on the `Evaluation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Evaluation" DROP COLUMN "hasStar";
--- prisma/migrations/20230427090653_remove_test_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `test` on the `Evaluation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Evaluation" DROP COLUMN "test";
