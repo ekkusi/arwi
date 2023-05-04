063818a0f89c4858af65b5cb7a56f3f868a699e8
Ekku Sipilä <ekku.eki@gmail.com>
2023-05-04 16:14:21 +0300

    Remove automatic deploy from loadAndRestart

--- scripts/prod/loadAndRestart.sh
+++ scripts/prod/loadAndRestart.sh
@@ -7,7 +7,6 @@ cd "../../"
 
 git pull
 /bin/bash scripts/dumpdb.sh -b
-npx dotenv -e .env.production -- npx prisma migrate deploy
 docker pull ekkusi/arwi
 docker stop arwi || true && docker rm arwi || true
 docker run --env-file .env.production -d --name arwi -p 3000:3000 ekkusi/arwi
