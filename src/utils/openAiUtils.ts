ef6c5ac87482d01b24dfde08d1bba4a9f01f3495
Ekku Sipilä <ekku.eki@gmail.com>
2023-05-04 17:03:07 +0300

    Modify prompt text for feedback generation

--- src/utils/openAiUtils.ts
+++ src/utils/openAiUtils.ts
@@ -20,21 +20,7 @@ const OpenAIUtils_Evaluation_Fragment = graphql(`
 export async function generateStudentSummary(
   evaluationFragments: FragmentType<typeof OpenAIUtils_Evaluation_Fragment>[]
 ) {
-  const startMessage = `Olen kyseisen oppilaan liikunnanopettaja ja minun tulee antaa hänelle yleinen palaute,
-                        kehitysehdotus ja huomio vahvuusalueesta hänen suoritustensa perusteella. Päivämäärät,
-                        ympäristöt, taitojen ja työskentelyn arvosanat sekä opettajan huomiot on annettu listana.
-                        Arvosanat on annettu asteikoilla 6-10, joista 6 on heikoin ja 10 paras. Minun tulee
-                        tehdä vertailua työskentelyn ja taitojen välillä sekä eri ympäristöjen kesken, 
-                        mikäli arvosanoissa on selkeitä eroavaisuuksia. Jos oppilaan taidot tai työskentely
-                        on jossain ympäristössä 7 tai alle, minun pitää antaa siitä ympäristöstä selkeästi
-                        kehitettävää palautetta. Jos taidot tai työskentely taas on jossain ympäristössä 9 
-                        tai enemmän, minun pitää kehua oppilasta kyseisestä ympäristöstä. Pyydän 
-                        sinua kirjoittamaan palaute sinutellen seuraavaassa rakenteessa:\n\n
-                        Yleinen palaute: vähintään 50 sanaa\n\n
-                        Kehitysehdotus: vähintään 10 sanaa\n\n
-                        Vahvuutesi: vähintään 10 sanaa\n\n
-                        Tässä on lista oppilaan suorituksista:\n\n
-                        `;
+  const startMessage = `Olen kyseisen oppilaan liikunnanopettaja, ja minun tulee antaa hänelle yleinen palaute, kehitysehdotus ja huomio vahvuusalueesta hänen suoritustensa perusteella. Päivämäärät, ympäristöt, taitojen ja työskentelyn arvosanat sekä opettajan huomiot on annettu listana. Arvosanat on annettu asteikoilla 6-10, joista 6 on heikoin ja 10 paras. Minun tulee tehdä vertailua työskentelyn ja taitojen välillä sekä eri ympäristöjen kesken, mikäli arvosanoissa on selkeitä eroavaisuuksia. Jos oppilaan taidot tai työskentely on jossain ympäristössä 7 tai alle, minun pitää antaa siitä ympäristöstä selkeästi kehitettävää palautetta. Jos taidot tai työskentely taas on jossain ympäristössä 9 tai enemmän, minun pitää kehua oppilasta kyseisestä ympäristöstä. Pyydän sinua kirjoittamaan noin 50 sanaisen palautteen oppilaalle sinuttelevassa muodossa ja hampurilaismallin mukaisesti:\n\n Tässä on lista oppilaan suorituksista:\n\n`;
   const evaluations = getFragmentData(
     OpenAIUtils_Evaluation_Fragment,
     evaluationFragments
