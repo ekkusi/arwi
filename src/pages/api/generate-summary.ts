b7fda146a1008557971882cdbc7a6eea76c05b96
Ekku Sipilä <ekku.eki@gmail.com>
2023-05-11 14:07:14 +0300

    Add authentication check to generate-summary route

--- src/pages/api/generate-summary.ts
+++ src/pages/api/generate-summary.ts
@@ -1,10 +1,16 @@
 import { generateStudentSummary } from "@/utils/openAiUtils";
 import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const session = await getServerSession(req, res, authOptions);
+  if (!session) {
+    return res.status(401).json({ message: "Unauthenticated" });
+  }
   // Check for secret to confirm this is a valid request
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Invalid method" });
