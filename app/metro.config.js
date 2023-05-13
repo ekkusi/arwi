2d755643b78c586c101abf16ecc5d05f30c32e5d
Ekku Sipilä <ekku.eki@gmail.com>
2023-05-13 17:00:12 +0300

    Test

--- app/metro.config.js
+++ app/metro.config.js
@@ -9,6 +9,8 @@ const workspaceRoot = path.resolve(projectRoot, "..");
 
 const config = getDefaultConfig(projectRoot);
 
+const test ="test";
+
 // 1. Watch all files within the monorepo
 config.watchFolders = [workspaceRoot];
 // 2. Let Metro know where to resolve packages and in what order
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -11,10 +11,12 @@ const removeIgnoredFiles = async (files) => {
 module.exports = {
   "*.{js,jsx}": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
+    if (filesToLint.length === 0) return [];
     return [`prettier --write ${filesToLint}`, `eslint --config .eslintrc.json --max-warnings=0 ${filesToLint}`];
   },
   "*.{ts, tsx}": async (files) => {
     const filesToLint = await removeIgnoredFiles(files);
+    if (filesToLint.length === 0) return [];
     return [`prettier --write ${filesToLint}`, `eslint --config .eslintrc.json --max-warnings=0 ${filesToLint}`];
   },
 };
