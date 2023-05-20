795fddb0fb02b7a5e79357baf2e811cb31caea74
Ekku Sipilä <ekku.eki@gmail.com>
2023-05-20 18:54:02 +0300

    Return next project to its previus state

--- next/src/graphql-server/types/contextTypes.ts
+++ next/src/graphql-server/types/contextTypes.ts
@@ -2,13 +2,9 @@ import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Session } from "next-auth";
 
-export type UserInfo = {
-  email: string;
-};
-
 export type CustomContext = {
   prisma: PrismaClient;
   req?: NextApiRequest;
   res?: NextApiResponse;
-  user?: UserInfo;
+  user?: Session["user"];
 };
