64765e51eb6668b0709736bc43c14e7507e91ad3
Mika Sipilä <mikasip11@gmail.com>
2023-07-05 12:57:55 +0300

    add student field

--- app/containers/home/group/creation/GroupStudentCreationView.tsx
+++ app/containers/home/group/creation/GroupStudentCreationView.tsx
@@ -24,9 +24,16 @@ export default function GroupSubjectSelectionView({ navigation, route }: GroupVi
   const { students } = route.params;
   return (
     <CView style={{ flex: 1, padding: 15, alignItems: "center", justifyContent: "center", gap: 30, backgroundColor: "white" }}>
-      <CView>
+      <CView style={{ height: 50 }}>
         <FlatList data={students} renderItem={renderStudentItem} keyExtractor={(_, index) => index.toString()} numColumns={1} />
       </CView>
+      <CView style={{ height: 60, flexDirection: "row" }}>
+        <CTextInput style={{ flex: 5 }} placeholder="Uusi oppilas"></CTextInput>
+        <CButton style={{ flex: 1, position: "absolute", right: 20 }}>
+          <MaterialCommunityIcon name="plus" color={COLORS.white} />
+        </CButton>
+      </CView>
+
       <CButton style={{ position: "absolute", bottom: 20, right: 20 }} onPress={() => navigation.navigate("GroupSubjectSelectionView", {})}>
         <MaterialCommunityIcon name="arrow-right" color={COLORS.white} />
       </CButton>
