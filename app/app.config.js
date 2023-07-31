96c83b0696fbeefea6229af1c89a4b533603a39c
Ekku Sipilä <ekku.eki@gmail.com>
2023-07-31 14:48:10 +0300

    Add missing eas config and separate test and dev eas profiles

--- app/app.config.js
+++ app/app.config.js
@@ -31,6 +31,11 @@ module.exports = {
     favicon: "./assets/logo-new.jpg",
   },
   scheme: "arwi-app",
+  extra: {
+    eas: {
+      projectId: "630de596-103e-469d-968f-bd0339f5f4c8",
+    },
+  },
   plugins: [
     [
       "@react-native-voice/voice",
