72e2fd01ce770d3af3b350ab8e6fe544e9f7e7ae
Ekku Sipilä <ekku.eki@gmail.com>
2023-08-22 19:19:26 +0300

    Update prod build and load scripts

--- scripts/prod/buildAndPushToProd.sh
+++ scripts/prod/buildAndPushToProd.sh
@@ -5,7 +5,5 @@ parentPath=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parentPath"
 cd "../../"
 
-docker build --platform linux/amd64 -t ekkusi/arwi:latest .
-docker push ekkusi/arwi:latest
-
-ssh eke@199.247.31.107 "cd arwi && git pull && bash scripts/prod/loadAndRestart.sh"
+docker build --platform linux/amd64 -t ekkusi/arwi-backend:latest -f Dockerfile.backend .
+docker push ekkusi/arwi-backend:latest
--- scripts/prod/loadAndRestart.sh
+++ scripts/prod/loadAndRestart.sh
@@ -7,8 +7,8 @@ cd "../../"
 
 git pull
 /bin/bash scripts/dumpdb.sh -b
-docker pull ekkusi/arwi
-docker stop arwi || true && docker rm arwi || true
-docker run --env-file .env.production -d --name arwi -p 3000:3000 ekkusi/arwi
+docker pull ekkusi/arwi-backend
+docker stop arwi-backend || true && docker rm arwi-backend || true
+docker run --env-file .env.production -d --name arwi-backend -p 3000:4000 ekkusi/arwi-backend
 docker rmi $(docker images -f "dangling=true" -q --no-trunc)
 echo "New container running and old ones removed successfully!"
