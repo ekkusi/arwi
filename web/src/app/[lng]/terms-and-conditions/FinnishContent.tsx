e652b2eb3586fee49157d12f59e41d259a9b8b92
Ekku Sipilä <ekku.eki@gmail.com>
2023-10-05 16:23:24 +0200

    Add links to privacy policy page from terms and conditions

--- web/src/app/[lng]/terms-and-conditions/FinnishContent.tsx
+++ web/src/app/[lng]/terms-and-conditions/FinnishContent.tsx
@@ -1,4 +1,5 @@
 import { Text } from "@chakra-ui/react";
+import Link from "@/components/primitives/Link";
 
 export default function TermsAndConditionsContentFi() {
   return (
@@ -15,10 +16,11 @@ export default function TermsAndConditionsContentFi() {
         Nämä käyttöehdot (jatkossa &quot;Käyttöehdot&quot; tai &quot;Sopimus&quot;) sääntelevät suhdettamme Arwi Oy:n (jatkossa &quot;Yritys&quot;,
         &quot;Arwi&quot;, &quot;Me&quot; tai &quot;Meidän&quot;) ja sen palvelujen käyttäjien (jatkossa &quot;Asiakas&quot;, &quot;Käyttäjä&quot; tai
         &quot;Sinä&quot;) välillä. Käyttäessäsi palveluitamme vahvistat lukeneesi ja hyväksyväsi kaikki näissä Käyttöehdoissa esitetyt ehdot, mukaan
-        lukien tietosuojaseloste, kokonaisuudessaan. Sinun tulee olla oikeutettu edustamaan omaa organisaatiotasi (koulua, kuntaa tai muuta virallista
-        organisaatiota) ja hyväksyt olevasi valtuutettu tekemään tämä sopimus Asiakkaan puolesta. Sopimus tulee voimaan käyttäessäsi palvelua. Kaikki
-        Arwiin lisätyt uudet ominaisuudet tai työkalut kuuluvat myös näiden Käyttöehtojen piiriin. Mikäli et hyväksy Käyttöehtoja ja/tai
-        Yksityisyydensuojaa tai sinulla ei ole oikeutta tehdä sopimusta, sinun tulee välittömästi lopettaa palvelun käyttö.
+        lukien <Link href="/privacy-policy">tietosuojaseloste</Link>, kokonaisuudessaan. Sinun tulee olla oikeutettu edustamaan omaa organisaatiotasi
+        (koulua, kuntaa tai muuta virallista organisaatiota) ja hyväksyt olevasi valtuutettu tekemään tämä sopimus Asiakkaan puolesta. Sopimus tulee
+        voimaan käyttäessäsi palvelua. Kaikki Arwiin lisätyt uudet ominaisuudet tai työkalut kuuluvat myös näiden Käyttöehtojen piiriin. Mikäli et
+        hyväksy Käyttöehtoja ja/tai Yksityisyydensuojaa tai sinulla ei ole oikeutta tehdä sopimusta, sinun tulee välittömästi lopettaa palvelun
+        käyttö.
       </Text>
       <Text as="h2">Sopimuksen Kohde</Text>
       <Text>
@@ -39,24 +41,25 @@ export default function TermsAndConditionsContentFi() {
       <Text as="h2">Maksullisen Tilauksen Luominen</Text>
       <Text>
         Maksullisen tilauksen tekeminen Arwi-palveluun edellyttää käyttäjältä sitovaa tarjousta sopimuksen tekemiseksi. Rekisteröityessäsi sinun tulee
-        hyväksyä nämä Käyttöehdot ja Arwin tietosuojaseloste. Tarjouksen hyväksyminen voi tapahtua myös rekisteröitymisen yhteydessä kokeilutilauksen
-        yhteydessä.
+        hyväksyä nämä Käyttöehdot ja Arwin <Link href="/privacy-policy">tietosuojaseloste</Link>. Tarjouksen hyväksyminen voi tapahtua myös
+        rekisteröitymisen yhteydessä kokeilutilauksen yhteydessä.
       </Text>
       <Text as="h2">Tietosuoja (GDPR)</Text>
       <Text>
         Arwi-sovelluksen palvelussa käyttäjä toimii tietojenkäsittelijänä, ja Yritys toimii tietojenkäsittelijänä käyttäjän puolesta. Nämä Käyttöehdot
-        yhdessä tietosuojaselosteen kanssa muodostavat sopimuksen henkilötietojen käsittelystä. Lue myös tuo asiakirja huolellisesti.
+        yhdessä <Link href="/privacy-policy">tietosuojaselosteen</Link> kanssa muodostavat sopimuksen henkilötietojen käsittelystä. Lue myös tuo
+        asiakirja huolellisesti.
       </Text>
       <Text>
         Yritys toimii tietojenkäsittelijänä, kun se kerää tietoja asiakkaistaan ja asiakkaan yhteyshenkilöistä sekä käyttäjistään omassa
-        asiakasrekisterissään. Tämä on tarpeen asiakassuhteen ylläpitoa ja hoitoa varten. Tietosuojaselosteessa määritellään, mitä tietoja kerätään,
-        miten niitä käytetään ja säilytetään sekä mitkä ovat käyttäjän oikeudet omiin tietoihinsa.
+        asiakasrekisterissään. Tämä on tarpeen asiakassuhteen ylläpitoa ja hoitoa varten. <Link href="/privacy-policy">Tietosuojaselosteessa</Link>{" "}
+        määritellään, mitä tietoja kerätään, miten niitä käytetään ja säilytetään sekä mitkä ovat käyttäjän oikeudet omiin tietoihinsa.
       </Text>
       <Text as="h2">Käyttöoikeus</Text>
       <Text>
         Sopimuksen myötä Yritys myöntää Käyttäjälle rajoitetun, ei-yksinomaisen, ei-siirrettävän ja peruutettavissa olevan oikeuden käyttää
-        Arwi-sovellusta ja sen toiminnallisuuksia näiden Käyttöehtojen ja tietosuojaselosteen mukaisesti. Tätä oikeutta ei saa käyttää muihin
-        tarkoituksiin kuin mitä näissä Käyttöehdoissa on määritelty.
+        Arwi-sovellusta ja sen toiminnallisuuksia näiden Käyttöehtojen ja <Link href="/privacy-policy">tietosuojaselosteen</Link> mukaisesti. Tätä
+        oikeutta ei saa käyttää muihin tarkoituksiin kuin mitä näissä Käyttöehdoissa on määritelty.
       </Text>
       <Text as="h2">Vastuunrajoitus ja Takuiden Poissulkeminen</Text>
       <Text>
@@ -72,7 +75,8 @@ export default function TermsAndConditionsContentFi() {
       <Text as="h2">Sopimuksen Päättäminen</Text>
       <Text>
         Sopimus on voimassa toistaiseksi. Kumpikin osapuoli voi irtisanoa sopimuksen kirjallisesti milloin tahansa. Yritys voi irtisanoa sopimuksen
-        myös välittömästi, jos Käyttäjä rikkoo näitä Käyttöehtoja tai tietosuojaselostetta. Kaikki sopimuksen irtisanomiset on tehtävä kirjallisesti.
+        myös välittömästi, jos Käyttäjä rikkoo näitä Käyttöehtoja tai <Link href="/privacy-policy">tietosuojaselostetta</Link>. Kaikki sopimuksen
+        irtisanomiset on tehtävä kirjallisesti.
       </Text>
       <Text as="h2">Voimassaolo</Text>
       <Text>Tämä Käyttöehtojen versio on voimassa 25. syyskuuta 2023 alkaen ja korvaa kaikki aiemmat Käyttöehtojen versiot.</Text>
