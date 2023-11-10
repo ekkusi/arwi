9ec2a8dbb1f8b5a04b9826d6536bf270c5cde82d
Ekku Sipilä <ekku.eki@gmail.com>
2023-11-10 14:38:07 +0100

    Update app version

--- app/app.config.js
+++ app/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   name: "Arwi",
   slug: "arwi-app",
   owner: "arwi",
-  version: "1.0.9",
+  version: "1.1.0",
   orientation: "portrait",
   icon: "./assets/logo-new.png",
   userInterfaceStyle: "light",
@@ -25,7 +25,7 @@ module.exports = {
   },
   android: {
     package: "net.arwi.twa",
-    versionCode: 11, // Always needs to be incremented when creating new android release
+    versionCode: 12, // Always needs to be incremented when creating new android release
     adaptiveIcon: {
       foregroundImage: "./assets/logo-new.png",
       backgroundColor: "#FEFCFB",
@@ -38,7 +38,7 @@ module.exports = {
   updates: {
     url: "https://u.expo.dev/630de596-103e-469d-968f-bd0339f5f4c8",
   },
-  runtimeVersion: "1.0.2",
+  runtimeVersion: "1.0.3",
   extra: {
     eas: {
       projectId: "630de596-103e-469d-968f-bd0339f5f4c8",
