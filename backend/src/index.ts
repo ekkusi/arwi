10b27ef96a1d15dd502562835053e1456eefa9ac
Ekku Sipilä <ekku.eki@gmail.com>
2024-01-07 13:54:04 +0100

    Fix alias import in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,11 @@
+/* eslint-disable import/newline-after-import  */
+/* eslint-disable import/first  */
+// Set up paths for built JS code. Has to be done manually because of monorepo
+// NOTE: These need to be updated in case of new aliases are added to tsconfig.json
+import moduleAlias from "module-alias";
+import path from "path";
+moduleAlias.addAlias("@", path.join(__dirname, "..", "lib"));
+
 import dotenv from "dotenv";
 import createApp from "./app";
 
