ba84b2eb6144ab4c15c26510c64c5458dab5853e
Ekku Sipilä <ekku.eki@gmail.com>
2024-02-07 12:09:03 +0100

    Implement header auth to backend and use this in frontend instead of cookies

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { TokenSetParameters } from "openid-client";
-import { SESSION_NAME } from "../config";
+import { SESSION_ID_HEADER_NAME, SESSION_NAME } from "../config";
 import { UserSessionInfo } from "../types/contextTypes";
 
 export const logOut = (req: Request, res: Response): Promise<void> => {
@@ -18,4 +18,10 @@ export const initSession = (req: Request, userInfo: UserSessionInfo, tokenSet?: 
   req.session.tokenSet = tokenSet;
   req.session.userInfo = userInfo;
   req.session.createdAt = Date.now();
+};
+
+const API_TOKENS = process.env.API_TOKENS?.split(",") || [];
+
+export const isValidApiToken = (apiToken: string): boolean => {
+  return API_TOKENS.includes(apiToken);
 };
