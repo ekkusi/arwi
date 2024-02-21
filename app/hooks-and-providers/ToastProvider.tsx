2fd335577be36acd5c260737c37979b417fa4e42
Ekku Sipilä <ekku.eki@gmail.com>
2024-02-21 15:42:47 +0100

    Implement ToastProvider to support multiple toasts open at the same time

--- app/hooks-and-providers/ToastProvider.tsx
+++ app/hooks-and-providers/ToastProvider.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, createContext, useMemo, useState } from "react";
+import React, { ReactElement, createContext, useState } from "react";
 import { Platform } from "react-native";
 import { SlideInDown, SlideInUp, SlideOutDown, SlideOutUp } from "react-native-reanimated";
 import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
@@ -13,7 +13,7 @@ import { CViewStyle } from "../theme/types";
 type OpenToastProps = {
   closeTimeout?: number;
   placement?: "top" | "bottom";
-  type?: "success" | "error";
+  type?: "success" | "error" | "warning";
 };
 
 type ToastActionProps = {
@@ -21,9 +21,19 @@ type ToastActionProps = {
   label: string;
 };
 
+type ToastContent = string | ReactElement | ((closeToast: () => void) => ReactElement);
+
+type ToastMessage = {
+  content: ToastContent;
+  id: number;
+  props: OpenToastProps;
+  actionProps?: ToastActionProps;
+  timeoutRef: ReturnType<typeof setTimeout>;
+};
+
 type ToastContextType = {
-  openToast: (content: string, props?: OpenToastProps, actionProps?: ToastActionProps) => void;
-  closeToast: () => void;
+  openToast: (content: ToastContent, props?: OpenToastProps, actionProps?: ToastActionProps) => () => void;
+  closeAllToasts: () => void;
 };
 
 const ToastContext = createContext<ToastContextType | null>(null);
@@ -40,127 +50,172 @@ export const useToast = () => {
 
 const DEFAULT_CLOSE_TIMEOUT_MS = 5000;
 
+let toastId = 0;
+
 export default function ToastProvider({ children }: React.PropsWithChildren) {
-  const [toastProps, setToastProps] = useState<OpenToastProps | null>(null);
-  const [toastContent, setToastContent] = useState<string | ReactElement | null>(null);
-  const [toastActionProps, setToastActionProps] = useState<ToastActionProps | null>(null);
-  const closeTimer = React.useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [toasts, setToasts] = useState<ToastMessage[]>([]);
 
   const { t } = useTranslation();
 
-  const openToast = (content: string | ReactElement, props?: OpenToastProps, actionProps?: ToastActionProps) => {
-    setToastContent(content);
-    setToastProps(props || {});
-    setToastActionProps(actionProps || null);
-    closeTimer.current = setTimeout(() => {
-      closeToast();
-    }, props?.closeTimeout || DEFAULT_CLOSE_TIMEOUT_MS);
+  const openToast = (content: ToastContent, props?: OpenToastProps, actionProps?: ToastActionProps): (() => void) => {
+    toastId += 1;
+    const id = toastId; // Increment id for each new toast
+    const newToast: ToastMessage = {
+      content,
+      id,
+      props: props || { type: "success", placement: "bottom" },
+      actionProps,
+      timeoutRef: setTimeout(() => closeToast(id), props?.closeTimeout || DEFAULT_CLOSE_TIMEOUT_MS),
+    };
+    setToasts((currentToasts) => [...currentToasts, newToast]);
+
+    // Return a function that allows closing this specific toast without needing the id externally
+    return () => {
+      closeToast(id);
+    };
   };
 
-  const closeToast = () => {
-    setToastContent(null);
-    setToastProps(null);
-    setToastActionProps(null);
-    if (closeTimer.current) clearInterval(closeTimer.current);
+  const closeToast = (id: number) => {
+    setToasts((currentToasts) => {
+      // Find the toast to clear its timeout before filtering it out
+      const toastToClose = currentToasts.find((toast) => toast.id === id);
+      if (toastToClose) {
+        clearTimeout(toastToClose.timeoutRef);
+      }
+      return currentToasts.filter((toast) => toast.id !== id);
+    });
   };
 
-  const handleActionPress = () => {
-    toastActionProps?.action();
-    closeToast();
+  const closeAllToasts = () => {
+    setToasts((currentToasts) => {
+      currentToasts.forEach((toast) => {
+        if (toast.timeoutRef) {
+          clearTimeout(toast.timeoutRef);
+        }
+      });
+      return [];
+    });
   };
 
-  const placement = toastProps?.placement || "bottom";
-  const type = toastProps?.type || "success";
+  const handleActionPress = (toast: ToastMessage) => {
+    toast.actionProps?.action();
+    closeToast(toast.id);
+  };
 
-  const placementStyles: CViewStyle = useMemo(() => {
+  const getToastAnimations = (placement: OpenToastProps["placement"]) => {
+    const exitAnimation = placement === "top" ? SlideOutUp : SlideOutDown;
+    const enterAnimation = placement === "top" ? SlideInUp : SlideInDown;
+
+    return { exiting: exitAnimation, entering: enterAnimation };
+  };
+
+  const getToastStyles = (toastProps: OpenToastProps) => {
+    const { placement, type } = toastProps;
+
     let styles: CViewStyle;
-    switch (placement) {
-      case "top":
-        styles = { top: Platform.OS === "ios" ? "4xl" : "2xl" };
+
+    switch (type) {
+      case "error":
+        styles = { backgroundColor: "error" };
+        break;
+      case "warning":
+        styles = { backgroundColor: "yellow" };
         break;
       default:
-        styles = { bottom: Platform.OS === "ios" ? "2xl" : "xl" }; // iOS bottom part of screen tends to be more rounded so tooltip has to be a little big higher
+        styles = { backgroundColor: "green" };
+    }
+
+    switch (placement) {
+      case "top":
+        styles = { top: Platform.OS === "ios" ? "4xl" : "2xl", ...styles };
+        break;
+      default:
+        styles = { bottom: Platform.OS === "ios" ? "2xl" : "xl", ...styles }; // iOS bottom part of screen tends to be more rounded so tooltip has to be a little big higher
         break;
     }
     return styles;
-  }, [placement]);
+  };
 
-  const exitAnimation = useMemo(() => {
-    switch (placement) {
-      case "top":
-        return SlideOutUp;
+  const getToastActionButtonColor = (type: OpenToastProps["type"]) => {
+    switch (type) {
+      case "error":
+        return "error";
+      case "warning":
+        return "yellow";
       default:
-        return SlideOutDown;
+        return "darkgreen";
     }
-  }, [placement]);
+  };
 
-  const enterAnimation = useMemo(() => {
-    switch (placement) {
-      case "top":
-        return SlideInUp;
+  const getToastIcon = (type: OpenToastProps["type"]) => {
+    switch (type) {
+      case "error":
+        return "exclamation";
       default:
-        return SlideInDown;
+        return "check";
     }
-  }, [placement]);
+  };
+
+  const renderContent = (toast: ToastMessage) => {
+    if (typeof toast.content === "string") {
+      return <CText style={{ color: "white", flex: 1, marginRight: "2xl" }}>{toast.content}</CText>;
+    }
+    if (typeof toast.content === "function") {
+      return toast.content(() => closeToast(toast.id));
+    }
+    return toast.content;
+  };
 
   return (
     <Provider
       value={{
         openToast,
-        closeToast,
+        closeAllToasts,
       }}
     >
       {children}
       {/* Wrapper View is needed to make exit animation work in iOS */}
-      <CView style={{ position: "absolute", bottom: 0, right: 0, left: 0, top: 0 }} pointerEvents="box-none">
-        {toastContent && toastProps && (
-          <CAnimatedView
-            entering={enterAnimation}
-            exiting={exitAnimation}
-            style={{
-              position: "absolute",
-              right: "md",
-              left: "md",
-              alignItems: "center",
-              backgroundColor: type === "success" ? "green" : "error",
-              padding: "lg",
-              borderRadius: 10,
-              ...placementStyles,
-            }}
-          >
-            <CView
+      <CView
+        style={{ position: "absolute", bottom: 0, right: 0, left: 0, top: 0, justifyContent: "flex-end", gap: "lg", paddingHorizontal: "md" }}
+        pointerEvents="box-none"
+      >
+        {toasts
+          .map((toast) => (
+            <CAnimatedView
+              key={toast.id}
               style={{
-                flexDirection: "row",
                 alignItems: "center",
+                padding: "lg",
+                borderRadius: 10,
+                ...getToastStyles(toast.props),
               }}
+              {...getToastAnimations(toast.props.placement)}
             >
-              <MaterialCommunityIcon
-                name={type === "success" ? "check" : "exclamation"}
-                size={25}
-                color={COLORS.white}
-                style={{ marginRight: SPACING.md }}
-              />
-              {typeof toastContent === "string" ? (
-                <CText style={{ color: "white", flex: 1, marginRight: "2xl" }}>{toastContent}</CText>
-              ) : (
-                toastContent
+              <CView
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <MaterialCommunityIcon name={getToastIcon(toast.props.type)} size={25} color={COLORS.white} style={{ marginRight: SPACING.md }} />
+                {renderContent(toast)}
+                <CButton variant="empty" onPress={() => closeToast(toast.id)} style={{ position: "absolute", right: "sm", top: -10 }}>
+                  <MaterialCommunityIcon name="close" size={20} color={COLORS.white} />
+                </CButton>
+              </CView>
+              {toast.actionProps && (
+                <CButton
+                  size="small"
+                  variant="outline"
+                  colorScheme={getToastActionButtonColor(toast.props.type)}
+                  title={toast.actionProps.label || t("ok", "OK")}
+                  onPress={() => handleActionPress(toast)}
+                  style={{ borderWidth: 2, marginTop: "md" }}
+                />
               )}
-              <CButton variant="empty" onPress={closeToast} style={{ position: "absolute", right: "sm", top: -10 }}>
-                <MaterialCommunityIcon name="close" size={20} color={COLORS.white} />
-              </CButton>
-            </CView>
-            {toastActionProps && (
-              <CButton
-                size="small"
-                variant="outline"
-                colorScheme={type === "success" ? "darkgreen" : "error"}
-                title={toastActionProps.label || t("ok", "OK")}
-                onPress={handleActionPress}
-                style={{ borderWidth: 2, marginTop: "md" }}
-              />
-            )}
-          </CAnimatedView>
-        )}
+            </CAnimatedView>
+          ))
+          .reverse()}
       </CView>
     </Provider>
   );
