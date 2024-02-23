5fd5562f5e3d9b44c87720ae627989d414430689
mikasip <mikasip11@gmail.com>
2024-02-23 16:17:22 +0200

    generate feedback layouts

--- app/app/home/final-feedback/FinalFeedback.tsx
+++ app/app/home/final-feedback/FinalFeedback.tsx
@@ -2,6 +2,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useQuery } from "@apollo/client";
 import { useTranslation } from "react-i18next";
 import { hasRequiredField } from "arwi-backend/src/types/typeGuards";
+import MaterialCommunityIcon from "react-native-vector-icons/MaterialCommunityIcons";
 import CText from "../../../components/primitives/CText";
 import CView from "../../../components/primitives/CView";
 import { HomeStackParams } from "../types";
@@ -14,12 +15,15 @@ import CFlatList from "../../../components/primitives/CFlatList";
 import Card from "../../../components/Card";
 import Layout from "../../../components/Layout";
 import { useToggleTokenUseWarning } from "../../../hooks-and-providers/monthlyTokenUseWarning";
+import EvaluationTargetCard from "../group/components/EvaluationTargetCard";
+import { COLORS } from "../../../theme";
 
 const FinalFeedback_GetGroup_Query = graphql(`
   query FinalFeedback_GetGroup($groupId: ID!) {
     getGroup(id: $groupId) {
       id
       name
+      archived
       students {
         id
         name
@@ -27,9 +31,20 @@ const FinalFeedback_GetGroup_Query = graphql(`
           id
           text
         }
+        currentModuleEvaluations {
+          id
+          notes
+        }
       }
       currentModule {
         id
+        evaluationCollections {
+          id
+          type {
+            id
+            category
+          }
+        }
         collectionTypes {
           id
           category
@@ -69,10 +84,7 @@ export default function FinalFeedback({ route, navigation }: NativeStackScreenPr
   const group = data.getGroup;
 
   const otherCollectionTypes = group.currentModule.collectionTypes.filter((type) => type.category !== "CLASS_PARTICIPATION");
-  const evaluatedOtherCollectionTypes = otherCollectionTypes.filter((type) => hasRequiredField(type, "defaultTypeCollection")) as WithRequiredNonNull<
-    (typeof otherCollectionTypes)[number],
-    "defaultTypeCollection"
-  >[];
+  const nonEvaluatedOtherCollectionTypes = otherCollectionTypes.filter((type) => !hasRequiredField(type, "defaultTypeCollection"));
 
   const studentsWithFeedback = group.students.filter((student) => hasRequiredField(student, "latestFeedback")) as WithRequiredNonNull<
     (typeof group.students)[number],
@@ -108,34 +120,79 @@ export default function FinalFeedback({ route, navigation }: NativeStackScreenPr
     );
   };
 
+  const studentsWithLessThanThreeVerbalFeedback = group.students.filter(
+    (student) => student.currentModuleEvaluations.filter((ev) => ev.notes).length < 3
+  );
+  const studentsWithLessThanThreeEvaluations = group.students.filter((student) => student.currentModuleEvaluations.length < 3);
+
+  const allOtherCollectionTypesEvaluated = nonEvaluatedOtherCollectionTypes.length === 0;
+  const allStudentsHaveThreeEvaluations = !(studentsWithLessThanThreeEvaluations.length > 0);
+  const allStudentsHaveThreeVerbalFeedback = !(studentsWithLessThanThreeVerbalFeedback.length > 0);
+
   return (
-    <Layout style={{ paddingHorizontal: "md", paddingVertical: "lg" }}>
-      <CText style={{ fontSize: "xl", marginBottom: "xl" }}>Loppupalaute koko ryhmälle</CText>
+    <Layout style={{ paddingHorizontal: "md", paddingVertical: "lg", backgroundColor: "white", gap: "lg" }}>
+      <CText style={{ fontSize: "title", fontWeight: "500" }}>{group.name} - Loppupalaute</CText>
       {studentsWithFeedback.length === 0 ? (
-        <CView style={{ gap: 10 }}>
-          <CText style={{ fontSize: "sm", fontWeight: "300" }}>
-            {t(
-              "group-final-feedback-info",
-              "Generoi loppupalaute koko ryhmälle perustuen antamiisi numeerisiin ja sanallisiin palautteisiin. Palautteen generointi tapahtuu taustalla, voit liikkua sovelluksessa vapaasti tai sulkea sovelluksen generoinnin ajaksi."
-            )}
+        <CView style={{ gap: "xl" }}>
+          <CText>
+            <CText style={{ fontSize: "sm2", fontWeight: "300" }}>
+              Palaute generoidaan perustuen antamiisi numeerisiin ja sanallisiin palautteisiin. Olet tehnyt ryhmälle yhteensä{" "}
+            </CText>
+            <CText style={{ fontSize: "sm2", fontWeight: "500" }}>
+              {t("class-evaluation-count", "{{count}} tuntiarviointia", {
+                count: group.currentModule.evaluationCollections.filter((ev) => ev.type.category === "CLASS_PARTICIPATION").length,
+              })}{" "}
+            </CText>
+            <CText style={{ fontSize: "sm2", fontWeight: "300" }}>ja antanut sanallista palautetta keskimäärin </CText>
+            <CText style={{ fontSize: "sm2", fontWeight: "500" }}>0 kertaa per oppilas.</CText>
           </CText>
-          {evaluatedOtherCollectionTypes.length !== otherCollectionTypes.length && (
-            <CView style={{ gap: 5 }}>
-              <CText style={{ fontSize: "sm", fontWeight: "500" }}>
-                {t("other-type-evaluations-missing", "HUOM! Seuraavat arviointikohteet ovat arvioimatta:")}
+          <CView style={{ gap: "xl" }}>
+            <CView style={{ flexDirection: "row", gap: "lg", alignItems: "center" }}>
+              <MaterialCommunityIcon
+                name={!allOtherCollectionTypesEvaluated ? "alert-outline" : "check-bold"}
+                size={40}
+                color={!allOtherCollectionTypesEvaluated ? COLORS.red : COLORS.primary}
+              />
+              <CText style={{ width: "80%", fontSize: "sm", fontWeight: "300" }}>
+                {allOtherCollectionTypesEvaluated
+                  ? "Kaikki arvioitavat sisällöt on arvioitu!"
+                  : `Ryhmällä on ${nonEvaluatedOtherCollectionTypes.length} arvioitava sisältö arvioimatta. Arvioi sisältö ennen palautteen generointia.`}
               </CText>
-              <CView>
-                {evaluatedOtherCollectionTypes.map((type) => {
-                  return (
-                    <CText key={type.id} style={{ fontSize: "sm", fontWeight: "500" }}>
-                      {type.name}
-                    </CText>
-                  );
-                })}
-              </CView>
             </CView>
-          )}
-          <CButton title={t("generate", "Generoi")} size="small" onPress={generateFinalFeedback} />
+            <CView style={{ flexDirection: "row", gap: "lg", alignItems: "center" }}>
+              <MaterialCommunityIcon
+                name={!allStudentsHaveThreeEvaluations ? "alert-rhombus-outline" : "check-bold"}
+                size={40}
+                color={!allStudentsHaveThreeEvaluations ? COLORS.yellow : COLORS.primary}
+              />
+              <CText style={{ width: "80%", fontSize: "sm", fontWeight: "300" }}>
+                {allStudentsHaveThreeEvaluations
+                  ? "Kaikilla ryhmän oppilailla on riittävästi arviointeja!"
+                  : `Ryhmässä on ${studentsWithLessThanThreeEvaluations.length} oppilasta, joilla on alle 3 arviointia. Kyseisille oppilaille ei ole mahdollista generoida sanallista palautetta.`}
+              </CText>
+            </CView>
+            <CView style={{ flexDirection: "row", gap: "lg", alignItems: "center" }}>
+              <MaterialCommunityIcon
+                name={!allStudentsHaveThreeVerbalFeedback ? "alert-rhombus-outline" : "check-bold"}
+                size={40}
+                color={!allStudentsHaveThreeVerbalFeedback ? COLORS.yellow : COLORS.primary}
+              />
+              <CText style={{ width: "80%", fontSize: "sm", fontWeight: "300" }}>
+                {allStudentsHaveThreeVerbalFeedback
+                  ? "Kaikille ryhmän oppilaille on annettu riittävästi sanallista palautetta!"
+                  : `Ryhmässä on ${studentsWithLessThanThreeVerbalFeedback.length} oppilasta, joille on annettu alle 3 sanallista palautetta. Antamalla sanallista palautetta yksilöllisempien palautteiden generointi on mahdollista.`}
+              </CText>
+            </CView>
+            <CView style={{ gap: "md" }}>
+              <CText style={{ fontSize: "sm2", fontWeight: "500" }}>Kaikki valmiina loppupalautteen generoimiseksi!</CText>
+            </CView>
+            <CView style={{ gap: "sm" }}>
+              <CButton title={t("generate", "Generoi")} onPress={generateFinalFeedback} disabled={!allOtherCollectionTypesEvaluated} />
+              <CText style={{ fontSize: "sm", fontWeight: "300" }}>
+                Palautteen generointi tapahtuu taustalla, voit liikkua sovelluksessa vapaasti tai sulkea sovelluksen generoinnin ajaksi.
+              </CText>
+            </CView>
+          </CView>
         </CView>
       ) : (
         <CFlatList
