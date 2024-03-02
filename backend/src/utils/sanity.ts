4ccb436ab567e9838a499434fc7a4c0566053911
Ekku Sipilä <ekku.eki@gmail.com>
2024-03-02 11:38:47 +0100

    Remove console.log

--- backend/src/utils/sanity.ts
+++ backend/src/utils/sanity.ts
@@ -19,8 +19,6 @@ export const getAgreements = async (): Promise<Agreement[]> => {
 };
 
 export const hasAgreement = async (oids: string[]): Promise<boolean> => {
-  console.log("hasAgreement real called");
-
   const query = `*[_type == "mpassid_agreement" && info.oid in $oids]`;
   const agreement = await client.fetch(query, { oids });
   return agreement.length > 0;
